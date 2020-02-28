2ade9ef87e4c4c28117c92b8e16880287f74af49
dannyxx17 <57616358+dannyxx17@users.noreply.github.com>
2020-02-28 10:11:01 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>hospital software</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-light navbar-expand-md fixed-top shadow navigation-clean-button" style="height: 55px;font-family: roboto;">
+        <div class="container"><a class="navbar-brand" href="#">FutmxClinic</a><button data-toggle="collapse" class="navbar-toggler text-center bg-info border rounded border-primary shadow-sm" data-target="#navcol-1" style="background-color: rgb(104,49,241);"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+            <div
+                class="collapse navbar-collapse" id="navcol-1">
+                <ul class="nav navbar-nav mx-auto">
+                    <li class="nav-item" role="presentation"><a class="nav-link active" href="#">Blog</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link active" href="#">Book An Appointment</a></li>
+                    <li class="nav-item dropleft"><a class="dropdown-toggle nav-link stretched-link" data-toggle="dropdown" aria-expanded="false" href="#">Medical areas</a>
+                        <div class="dropdown-menu" role="menu"><a class="dropdown-item" role="presentation" href="#">First Item</a><a class="dropdown-item" role="presentation" href="#">Second Item</a><a class="dropdown-item" role="presentation" href="#">Third Item</a></div>
+                    </li>
+                    <li class="nav-item dropright"><a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false" href="#">Solutios for</a>
+                        <div class="dropdown-menu" role="menu"><a class="dropdown-item" role="presentation" href="#">First Item</a><a class="dropdown-item" role="presentation" href="#">Second Item</a><a class="dropdown-item" role="presentation" href="#">Third Item</a></div>
+                    </li>
+                </ul><span class="navbar-text actions"> <a class="login" href="login.php">Log In</a><a class="btn btn-light action-button" role="button" href="#" style="background-color: rgb(176,41,224);">Register</a></span></div>
+        </div>
+    </nav>
+    <div id="banner">
+        <div class="jumbotron jumbotron-fluid text-center bg-white border rounded shadow-sm" style="padding-right: 30px;margin-top: 84px;margin-left: 20px;margin-right: 20px;background-color: transparent;background-image: url(&quot;assets/img/home-banner.jpg&quot;);background-repeat: no-repeat;background-position: 50% 50%;background-size: cover;">
+            <h1>Welcome</h1>
+            <p>Welcome to Futminna clinic where bla bla bla bla bla bla bla bla</p>
+            <p><a class="btn btn-primary" role="button" style="background-color: rgb(176,41,224);">Visit</a></p>
+        </div>
+    </div>
+    <div class="container-fluid">
+        <div class="row text-center justify-content-center">
+            <div class="col-4" id="med">
+                <div class="jumbotron border rounded shadow-sm" style="width: 300px;height: 276px;padding-top: 10px;background-color: rgb(255,255,255);">
+                    <h3><i class="fa fa-stethoscope" style="color: rgb(176,41,224);"></i>Emergency Services</h3>
+                    <p>Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam.</p>
+                    <p><a class="btn btn-primary" role="button" style="background-color: rgb(195,174,250);">Learn more</a></p>
+                </div>
+            </div>
+            <div class="col-4" id="med">
+                <div class="jumbotron border rounded shadow-sm" style="width: 300px;height: 276px;padding-top: 10px;background-color: rgb(255,255,255);">
+                    <h3><i class="fa fa-hospital-o" style="color: rgb(176,41,224);"></i>Doctor's schedule</h3>
+                    <p>Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam.</p>
+                    <p><a class="btn btn-primary" role="button" style="background-color: rgb(195,174,250);">Learn more</a></p>
+                </div>
+            </div>
+            <div class="col-4" id="med">
+                <div class="jumbotron border rounded shadow-sm" style="width: 300px;height: 276px;padding-top: 10px;background-color: rgb(255,255,255);">
+                    <h3><i class="fa fa-thermometer-three-quarters" style="color: rgb(176,41,224);"></i>Online Appointment</h3>
+                    <p>Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam.</p>
+                    <p><a class="btn btn-primary" role="button" style="background-color: rgb(195,174,250);">Learn more</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div>
+            <h1 class="text-capitalize text-center">Make an appointment</h1>
+        </div>
+        <div>
+            <p class="text-center">Book sessions with our doctors.</p>
+            <div class="jumbotron jumbotron-fluid shadow" style="background-color: rgb(255,255,255);">
+                <div class="text-center" style="padding-bottom: 10px;"><label>Student &nbsp; &nbsp; &nbsp;Name: &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;<input type="text" placeholder="Enter name" autocomplete="on" style="max-width: 295px;width: 350px;"></label></div>
+                <div class="text-center"
+                    style="padding-bottom: 10px;"><label>Enter department: &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;<input type="text" placeholder="Enter name" autocomplete="on" style="max-width: 295px;width: 350px;"></label></div>
+                <div class="text-center" style="padding-bottom: 10px;"><label>Appointment Date: &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;<input type="text" placeholder="Enter name" autocomplete="on" style="max-width: 295px;width: 350px;"></label></div>
+                <div class="text-center" style="padding-bottom: 0px;"><label>Enter Department: &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;<input type="text" placeholder="Enter name" autocomplete="on" style="max-width: 295px;width: 350px;"></label></div>
+                <div class="text-center" style="padding-bottom: 0px;">
+                    <h4>Complaint message:</h4><textarea class="form-control-lg" style="margin-top: 0px;" placeholder="Tell us your issues..." cols="20" rows="40"></textarea></div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-dark">
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-6 col-md-3 item">
+                        <h3>CONNECT WITH US</h3>
+                        <p>Federal university of Technology Minna, Main Campus, Gidan kwanu P.M.B 65, Minna, Niger State Nigeria</p>
+                        <ul>
+                            <li><a href="#">Phone no 1</a></li>
+                            <li><a href="#">Phone no 2</a></li>
+                            <li><a href="#">Phone no 3</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-6 col-md-3 item">
+                        <h3>Latest Update</h3>
+                        <ul>
+                            <li><a href="#">Update 1</a></li>
+                            <li><a href="#">Update 2</a></li>
+                            <li><a href="#">Update 3</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-6 item text">
+                        <h3>Quick Access;</h3>
+                        <ul>
+                            <li>Staff</li>
+                            <li>Students</li>
+                            <li>Alumni</li>
+                            <li>parents</li>
+                            <li>Publication</li>
+                        </ul>
+                    </div>
+                </div>
+                <p class="copyright">@COPYRIGHT FEDERAL UNIVERSITY OF TECHNOLOGY MINNA  <?php echo date("l-M-Y");?>, DESIGN &amp; DEPLOYMENT BY GROUP 3</p>
+            </div>
+        </footer>
+    </div>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
