99bdfc3ee72b36c5258b5d09cebd673538d24a9b
dannyxx17 <57616358+dannyxx17@users.noreply.github.com>
2020-02-28 10:11:46 +0100

    Add files via upload

---
+++ login.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>hospital software</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-light navbar-expand-md fixed-top shadow navigation-clean-button" style="height: 55px;font-family: roboto;">
+        <div class="container"><a class="navbar-brand" href="#">FutmxClinic</a><button data-toggle="collapse" class="navbar-toggler text-center bg-info border rounded border-primary shadow-sm" data-target="#navcol-1" style="background-color: rgb(104,49,241);"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+            <div
+                class="collapse navbar-collapse" id="navcol-1">
+                <ul class="nav navbar-nav mx-auto">
+                    <li class="nav-item" role="presentation"><a class="nav-link active" href="#">Blog</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="#">Book An Appointment</a></li>
+                    <li class="nav-item dropleft"><a class="dropdown-toggle nav-link stretched-link" data-toggle="dropdown" aria-expanded="false" href="#">Medical areas</a>
+                        <div class="dropdown-menu" role="menu"><a class="dropdown-item" role="presentation" href="#">First Item</a><a class="dropdown-item" role="presentation" href="#">Second Item</a><a class="dropdown-item" role="presentation" href="#">Third Item</a></div>
+                    </li>
+                    <li class="nav-item dropright"><a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false" href="#">Solutios for</a>
+                        <div class="dropdown-menu" role="menu"><a class="dropdown-item" role="presentation" href="#">First Item</a><a class="dropdown-item" role="presentation" href="#">Second Item</a><a class="dropdown-item" role="presentation" href="#">Third Item</a></div>
+                    </li>
+                </ul><span class="navbar-text actions"> <a class="login" href="#">Log In</a><a class="btn btn-light action-button" role="button" href="#" style="background-color: rgb(176,41,224);">Register</a></span></div>
+        </div>
+    </nav>
+    <div class="login-dark" style="background-image: url(&quot;assets/img/home-banner.jpg&quot;);background-position: 50% 50%;background-size: auto;background-repeat: no-repeat;background-color: transparent;">
+        <form method="post" style="background-color: rgb(245,249,252);">
+            <h2 class="sr-only">Login Form</h2>
+            <div class="illustration"><i class="icon ion-ios-locked-outline"></i></div>
+            <div class="form-group"><input class="form-control" type="email" name="Student id" placeholder="Student id" autocomplete="on" required="" minlength="8"></div>
+            <div class="form-group"><input class="form-control" type="password" name="password" placeholder="Password"></div>
+            <div class="form-group"><button class="btn btn-primary btn-block" type="submit">Log In</button></div><a class="forgot" href="#">Forgot your email or password?</a></form>
+    </div>
+    <div class="footer-dark">
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-6 col-md-3 item">
+                        <h3>CONNECT WITH US</h3>
+                        <p>Federal university of Technology Minna, Main Campus, Gidan kwanu P.M.B 65, Minna, Niger State Nigeria</p>
+                        <ul>
+                            <li><a href="#">Phone no 1</a></li>
+                            <li><a href="#">Phone no 2</a></li>
+                            <li><a href="#">Phone no 3</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-6 col-md-3 item">
+                        <h3>Latest Update</h3>
+                        <ul>
+                            <li><a href="#">Update 1</a></li>
+                            <li><a href="#">Update 2</a></li>
+                            <li><a href="#">Update 3</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-6 item text">
+                        <h3>Quick Access&nbsp;</h3>
+                        <ul>
+                            <li>Staff</li>
+                            <li>Students</li>
+                            <li>Alumni</li>
+                            <li>parents</li>
+                            <li>Publication</li>
+                        </ul>
+                    </div>
+                </div>
+               <p class="copyright">@COPYRIGHT FEDERAL UNIVERSITY OF TECHNOLOGY MINNA  <?php echo date("l-M-Y");?>, DESIGN &amp; DEPLOYMENT BY GROUP 3</p>
+            </div>
+           
+        </footer>
+    </div>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
